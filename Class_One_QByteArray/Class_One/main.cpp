3eca52de2305d2591d98f58d57f192232dbb5cb6
鸢一_Zoey <1142730150@qq.com>
2022-02-28 19:28:32 +0800

    更新QByteArray

--- Class_One_QByteArray/Class_One/main.cpp
+++ Class_One_QByteArray/Class_One/main.cpp
@@ -52,7 +52,69 @@ int main(int argc, char *argv[])
     msg_4.data()[3] = 'D';
     msg_4.data()[4] = 'E';
 
+    qDebug()<<msg_4[0];
+    qDebug()<<msg_4.data()[1];
+    qDebug()<<msg_4.at(2);
+    qDebug()<<msg_4.constData()[3];
+    qDebug()<<msg_4.constData()[4];
+    qDebug()<<msg_4.constData()[5];
 
+    //5. 截取字符串
+    //left()   right()    mid()
+    //left(int len)：从索引0开始，截取长度len的片段。len超过数组长度，则返回字符数组
+    //right(int len)：从索引尾部开始，截取长度len的片段。len超过数组长度，则返回字符数组
+    //mid(int pos, int len = -1)：如果len = -1，或者pos+len大于数组长度，则返回整个字符数组，否则从pos开始，截取len长度的字符数组
+    QByteArray msg_5 = "ABCDEFG";
+    QByteArray msg_5_left = msg_5.left(3);
+    qDebug()<<msg_5_left;
+    QByteArray msg_5_right = msg_5.right(3);
+    qDebug()<<msg_5_right;
+    QByteArray msg_5_mid = msg_5.mid(1,4);
+    qDebug()<<msg_5_mid;
+    qDebug()<<" ";
+
+    //6. 获取字节数组的大小
+    //int size()
+    //int length()
+    //int count()
+    //三个函数等价，遇到\0会被截断，停止统计，但是通过resize()函数开辟的内存，再手动填充的\0不会截断统计
+    QByteArray msg_6 = "Hello";
+    int msg_6_length = msg_6.size();
+    qDebug()<<msg_6_length;
+    QByteArray msg_6_resize;
+    msg_6_resize.resize(6);
+    msg_6_resize[0] = 'A';
+    msg_6_resize[1] = '\0';
+    msg_6_resize[2] = 'B';
+    msg_6_resize[3] = 'C';
+    msg_6_resize[4] = 'D';
+    msg_6_resize[5] = 'E';
+    int msg_6_count = msg_6_resize.count();
+    qDebug()<<msg_6_count;
+    qDebug()<<" ";
+
+    //7. Hex转换
+    //fromHex()：将数字字符串变化为16进制数，然后将16进制数转10进制数，然后转ASCII码得到字符
+    //toHex()：将16进制数变成字符串输出
+    QByteArray text = QByteArray::fromHex("517420697320677265617421");
+    qDebug()<<text.data();
+    qDebug()<<" ";
+    QByteArray msg_7;
+    msg_7.resize(3);
+    msg_7[0] = 0x11;
+    msg_7[1] = 0x12;
+    msg_7[2] = 0x36;
+    qDebug()<<msg_7.toHex();
+    qDebug()<<" ";
+
+    //8. 数值转换与输出
+    //::number(int n, int base = 10)
+    int n = 63;
+    qDebug()<<QByteArray::number(n);
+    qDebug()<<QByteArray::number(n, 16);
+    qDebug()<<QByteArray::number(n, 16).toUpper();
+    qDebug()<<QByteArray::number(n, 2);
+    qDebug()<<QByteArray::number(n, 8);
     /*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~QByteArray学习区域~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
 
     return a.exec();
